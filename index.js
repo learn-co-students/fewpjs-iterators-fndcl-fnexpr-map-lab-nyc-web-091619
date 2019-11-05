373078b4a5b66fe7d08af0e2bb326713be32a108
Danny Lee <dreamer@panix.com>
2019-11-05 10:15:18 -0500

    Done.

--- index.js
+++ index.js
@@ -11,17 +11,33 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// instantiate constant titleCased and create an anonymous function
+// with and create "returnArray" to hold the return from applying
+// .map to the "tutorials" array.  We set a block variable named
+// sentence and iterate through the array.
+
 const titleCased = () => {
   let returnArray = tutorials.map(sentence => {
+
+    // create a new variable to hold the split sentences where 
+    // there is a '' so we can work on each word (capitalize)
     let splitSentence = sentence.split(' ')
 
+    // loop through the array of words in "splitSentence" and
+    // assign the current array item "splitSentence[i] to the value 
+    // returned by changing the character at place '0' (first letter)
+    // and the attaching the return of slice(1) which gives us
+    // alll the letters of the word string except the first character
     for (let i=0; i < splitSentence.length; i++) {
       splitSentence[i] = splitSentence[i].charAt(0).toUpperCase() + splitSentence[i].slice(1)
     }
     // console.log(typeof(splitSentence.join(' ')))
+    // Join them back together and add a " " between each
     return splitSentence.join(' ')
   })
   // console.log(typeof(returnArray))
+  // not sure why .map is returning an Object instead of an array?!?
+  // "convert" to array and send back to caller.
   return Object.values(returnArray);
 }
 
